49c52960812163e179821274fe3eba641b54fd0a
jyothi-morla23 <jyothim2303@gmail.com>
2024-08-28 21:29:07 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, TopHeaderComponent],
+  declarations: [AppComponent, HeaderComponent, TopHeaderComponent, TopMenuComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
